5958bbb3ab2b3738422a9318659193510f408732
B503yk <Michael181480@gmail.com>
2025-06-20 11:37:04 +0000

    [automatic commit] 2025-06-20 11:37:04 UTC 175.45.84.198

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
+const session = require('express-session');
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+// session setup
+app.use(session({
+  secret: 'dogwalk_key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
