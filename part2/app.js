84d181c57d534d19186176ae9cc21ced0e76d6cf
B503yk <Michael181480@gmail.com>
2025-06-20 12:54:48 +0000

    [automatic commit] 2025-06-20 12:54:48 UTC 175.45.84.198

--- part2/app.js
+++ part2/app.js
@@ -24,7 +24,8 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Logout
+// Logout route
+app.post('/api/users/logout')
 
 // Export the app instead of listening here
 module.exports = app;
